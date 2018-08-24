306483778f87ee8b3fb6b68f46e3e0134daabece
Xavier Duncan <xduncan@pm.me>
2018-08-24 13:13:17 -0400

    add array of users

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 let server = express();
+// array of users
+let users = [
+    { 
+        id: '1',
+        name: '@theWebsiteKid',
+        email: 'xavierduncan@pm.me',
+        password: 'tokyo'
+    },
+    {
+        id: '2',
+        name: '@nybblr',
+        email: 'jonathan@digitalcrafts.com',
+        password: 'europe'
+    }
+]
 
 // listen on http://localhost:3000
 server.listen(3000);
